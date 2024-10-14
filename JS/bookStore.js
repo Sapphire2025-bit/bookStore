df2570a0efe4701865e6f3ca9dfdcfbbef52905b
Sapphire Frommer <atida.student.sf@gmail.com>
2024-10-14 11:11:02 +0300

    the basic book list is now done

--- JS/bookStore.js
+++ JS/bookStore.js
@@ -4,17 +4,25 @@ function displayBooksFromJson()
 {
     const booksContainer = document.getElementById("listContainer");
 
-    //erase everything (all old appends)
-    booksContainer.innerHTML = "";
-    booksData.forEach(book => {
-        let bookInfo = document.createElement("div");
-        bookInfo.innerHTML = `
-            <div>${book.id}</div>
-            <div id="title">${book.title}</div>
-            <div id="price">${book.price}</div>
-            <div id="read">Read</div>
-            <div id="update">Update</div>
-            <div id="delete">trash</div>
+    //erase everything (all old appends) and add the headers
+    booksContainer.innerHTML = `
+            <div class="bookHeader">Id</div>
+            <div class="bookHeader">Title</div>
+            <div class="bookHeader">Price</div>
+            <div class="bookHeader">Action</div>
+            <div class="bookHeader"></div>
+            <div class="bookHeader"></div>
+        `;
+
+    //fill with new data - books from JSON
+    booksData.books.forEach(book => {
+        booksContainer.innerHTML += `
+            <div class="id">${book.id}</div>
+            <div class="title">${book.title}</div>
+            <div class="price">₪${book.price}</div>
+            <div class="read">Read</div>
+            <div class="update">Update</div>
+            <div class="delete"></div>
         `;
     });
 }
